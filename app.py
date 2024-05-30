08f5ddb1b9859c2b05351e2fa138a359fd076cfd
hauntedcupoftea <andydchauhan@gmail.com>
2024-05-30 12:35:43 +0530

    import router and include them in the app

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from app.routing import rag, llm
 
 app = FastAPI()
+
+app.include_router(rag)
+app.include_router(llm)
 
 if __name__ == '__main__':
     import uvicorn
