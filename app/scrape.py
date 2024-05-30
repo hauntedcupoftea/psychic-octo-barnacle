751b756f39343c5d62a86fb90ea1b34f8e7031e3
hauntedcupoftea <andydchauhan@gmail.com>
2024-05-30 13:05:59 +0530

    add basic scrape text

--- app/scrape.py
+++ app/scrape.py
@@ -1,4 +1,14 @@
-import bs4
+from bs4 import BeautifulSoup
+import requests
 
-def get_website_text(url: str):
-    pass
+def get_wikipedia_text(url: str):
+    page = requests.get(url)
+    yield page.status_code
+    wikisoup = BeautifulSoup(page.content, 'html.parser')
+    yield wikisoup.prettify() # for now, this will be changed to just text content later.
+
+
+if __name__ == '__main__':
+    url = 'https://en.wikipedia.org/wiki/Luke_Skywalker'
+    status, content = get_wikipedia_text(url)
+    print(status, content)
