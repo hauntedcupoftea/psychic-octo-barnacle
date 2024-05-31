72d5aa85f735371598f604f2a0e17b6bb66e00a5
hauntedcupoftea <andydchauhan@gmail.com>
2024-05-31 18:58:27 +0530

    write code for API call to llama

--- app/scrape.py
+++ app/scrape.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup
+import transformers
+import torch
 import requests
 import dotenv
 
@@ -26,9 +28,15 @@ def get_embeddings(texts: list[str],
     response = requests.post(api_url, headers=headers, json={"inputs": texts, "options": {"wait_for_model": True}})
     return response.json()
 
+def query_llama(query: str, model_id:str=env['LLM_ID']):
+    pipeline = transformers.pipeline("text-generation", model=model_id, model_kwargs={"torch_dtype": torch.bfloat16}, 
+                                    use_auth_token=env['HF_TOKEN'], device_map="auto")
+    return pipeline(query)
+
 if __name__ == '__main__':
     url = 'https://en.wikipedia.org/wiki/Luke_Skywalker'
     content = get_wikipedia_text(url)
     print(content, sep='\n')
     embeddings = get_embeddings(content)
-    print(len(embeddings) + ', ' + len(embeddings[0]))
+    print(str(len(embeddings)) + ', ' + str(len(embeddings[0])))
+    print(query_llama("Hey how are you doing today?"))
