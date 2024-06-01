6424b49a40f71644691e9c6c0efb661bb76f7e9e
hauntedcupoftea <andydchauhan@gmail.com>
2024-06-01 18:17:41 +0530

    make barebones api

--- main_backend.py
+++ main_backend.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
-from app.routing import rag, llm
+from app.routing import qna
 
-app = FastAPI()
+app = FastAPI(
+    title='psychic-octo-barnacle', 
+    version='0.2.0'
+)
 
-app.include_router(rag)
-app.include_router(llm)
+app.include_router(qna)
+
+@app.get('/')
+def health():
+    return {
+        "message" : "Welcome to the Wikipedia Rag-Searching API!"
+    }
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, port='8000')
+    uvicorn.run(
+        app=app, 
+        port=8000,
+    )
