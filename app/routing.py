2c5bd55e7a2c60b36c05692c1f33f5df4c1edbf7
hauntedcupoftea <andydchauhan@gmail.com>
2024-06-01 17:36:34 +0530

    downscale routers to one category for now

--- app/routing.py
+++ app/routing.py
@@ -1,12 +1,7 @@
 """all fastapi routers go here"""
 from fastapi import APIRouter
 
-rag = APIRouter(
-    prefix='/rag',
-    tags=['rag']
-)
-
-llm = APIRouter(
-    prefix='/llm',
-    tags=['llm']
+qna = APIRouter(
+    prefix='/qna',
+    tags=['qna']
 )
